3c62c571439e9cf4ea840b2362d56e8acf550459
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-11 20:28:35 +0800

    - update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+import gradio as gr
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
+
+demo.launch()
